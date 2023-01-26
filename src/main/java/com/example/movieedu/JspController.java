9152140edabd084e422285ec5a8cce0ca7824c43
Üc207 Pr4 4f57t9 <120114154+4f57t9@users.noreply.github.com>
2023-01-26 12:53:04 +0900

    callAPI

--- src/main/java/com/example/movieedu/JspController.java
+++ src/main/java/com/example/movieedu/JspController.java
@@ -7,8 +7,12 @@ import model.vo.MovieVO;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import service.DailyBox;
+import service.WeeklyBox;
+
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
@@ -16,94 +20,116 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
-
 @Controller
 public class JspController {
 
+    @Autowired
+    DailyBox dailyBox;
+
+    @Autowired
+    WeeklyBox weeklyBox;
+
     @GetMapping("/")
-    public String callAPI(HttpSession s) {
+    public String callAPI(HttpSession s) throws InterruptedException {
 
-
-        try {
-
-           SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
-           Calendar d = Calendar.getInstance();
-           Calendar w = Calendar.getInstance();
-           d.add(Calendar.DATE, -1);
-           w.add(Calendar.DATE, -7);
-           w.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-           String dailydate = sdf.format(d.getTime());
-           String weeklydate = sdf.format(w.getTime());
-
-           String daily = RankingAPI.getUrl(dailydate);
-           String weekly = RankingWeekAPI.getUrl(weeklydate);
-
-           JSONObject dailybr = new JSONObject(daily);
-           JSONObject dbr = dailybr.getJSONObject("boxOfficeResult");
-           JSONArray dary = dbr.getJSONArray("dailyBoxOfficeList");
-
-           JSONObject weeklybr = new JSONObject(weekly);
-           JSONObject wbr = weeklybr.getJSONObject("boxOfficeResult");
-           JSONArray wary = wbr.getJSONArray("weeklyBoxOfficeList");
-
-           ArrayList<MovieVO> movielist = new ArrayList<MovieVO>();
-           String movieNm;
-
-           for (int i = 0; i < dary.length(); i++) {
-
-               s.setAttribute("movie", new MovieVO());
-               MovieVO movieVO = (MovieVO) s.getAttribute("movie");
-               movieNm = dary.getJSONObject(i).getString("movieNm");
-
-               String nAPI = NaverAPI.getUrl(movieNm);
-
-               JSONObject nr = new JSONObject(nAPI);
-               JSONArray items = nr.getJSONArray("items");
-
-               movieVO.setRank(dary.getJSONObject(i).getInt("rank"));
-
-               movieVO.setPubDate(items.getJSONObject(0).getInt("pubDate"));
-               movieVO.setSubtitle(items.getJSONObject(0).getString("subtitle"));
-               movieVO.setUserRating(items.getJSONObject(0).getDouble("userRating"));
-               movieVO.setDirector(items.getJSONObject(0).getString("director"));
-               movieVO.setActor(items.getJSONObject(0).getString("actor"));
-               movieVO.setImgUrl(items.getJSONObject(0).getString("image"));
-
-               movielist.add(movieVO);
-
-           }
-           Thread.sleep(1000);
-
-           for (int i = 10; i < (dary.length() + wary.length()); i++) {
-               s.setAttribute("movie", new MovieVO());
-               MovieVO movieVO = (MovieVO) s.getAttribute("movie");
-               movieNm = wary.getJSONObject((i - dary.length())).getString("movieNm");
-
-               String nAPI = NaverAPI.getUrl(movieNm);
-
-               JSONObject nr = new JSONObject(nAPI);
-               JSONArray items = nr.getJSONArray("items");
-
-               movieVO.setMovieNM(movieNm);
-               movieVO.setRank(wary.getJSONObject((i - dary.length())).getInt("rank"));
-
-               movieVO.setPubDate(items.getJSONObject(0).getInt("pubDate"));
-               movieVO.setSubtitle(items.getJSONObject(0).getString("subtitle"));
-               movieVO.setUserRating(items.getJSONObject(0).getDouble("userRating"));
-               movieVO.setDirector(items.getJSONObject(0).getString("director"));
-               movieVO.setActor(items.getJSONObject(0).getString("actor"));
-               movieVO.setImgUrl(items.getJSONObject(0).getString("image"));
-
-
-               movielist.add(movieVO);
-           }
+        ArrayList<MovieVO> movielist = new ArrayList<>();
+        movielist.addAll(dailyBox.getDailyBox(s));
+        Thread.sleep(1000);
+        movielist.addAll(weeklyBox.getWeeklyBox(s));
 
         s.setAttribute("movielist", movielist);
 
-        } catch (JSONException | InterruptedException e) {
-            e.printStackTrace();
-        }
         return "login";
     }
 }
+
+
+
+//@Controller
+//public class JspController {
+//
+//    @GetMapping("/")
+//    public String callAPI(HttpSession s) {
+//
+//        try {
+//
+//           SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+//           Calendar d = Calendar.getInstance();
+//           Calendar w = Calendar.getInstance();
+//           d.add(Calendar.DATE, -1);
+//           w.add(Calendar.DATE, -7);
+//           w.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+//           String dailydate = sdf.format(d.getTime());
+//           String weeklydate = sdf.format(w.getTime());
+//
+//           String daily = RankingAPI.getUrl(dailydate);
+//           String weekly = RankingWeekAPI.getUrl(weeklydate);
+//
+//           JSONObject dailybr = new JSONObject(daily);
+//           JSONObject dbr = dailybr.getJSONObject("boxOfficeResult");
+//           JSONArray dary = dbr.getJSONArray("dailyBoxOfficeList");
+//
+//           JSONObject weeklybr = new JSONObject(weekly);
+//           JSONObject wbr = weeklybr.getJSONObject("boxOfficeResult");
+//           JSONArray wary = wbr.getJSONArray("weeklyBoxOfficeList");
+//
+//           ArrayList<MovieVO> movielist = new ArrayList<MovieVO>();
+//           String movieNm;
+//
+//           for (int i = 0; i < dary.length(); i++) {
+//
+//               s.setAttribute("movie", new MovieVO());
+//               MovieVO movieVO = (MovieVO) s.getAttribute("movie");
+//               movieNm = dary.getJSONObject(i).getString("movieNm");
+//
+//               String nAPI = NaverAPI.getUrl(movieNm);
+//               JSONObject nr = new JSONObject(nAPI);
+//               JSONArray items = nr.getJSONArray("items");
+//
+//               movieVO.setRank(dary.getJSONObject(i).getInt("rank"));
+//
+//               movieVO.setPubDate(items.getJSONObject(0).getInt("pubDate"));
+//               movieVO.setSubtitle(items.getJSONObject(0).getString("subtitle"));
+//               movieVO.setUserRating(items.getJSONObject(0).getDouble("userRating"));
+//               movieVO.setDirector(items.getJSONObject(0).getString("director"));
+//               movieVO.setActor(items.getJSONObject(0).getString("actor"));
+//               movieVO.setImgUrl(items.getJSONObject(0).getString("image"));
+//
+//               movielist.add(movieVO);
+//
+//           }
+//           Thread.sleep(1000);
+//
+//           for (int i = 10; i < (dary.length() + wary.length()); i++) {
+//               s.setAttribute("movie", new MovieVO());
+//               MovieVO movieVO = (MovieVO) s.getAttribute("movie");
+//               movieNm = wary.getJSONObject((i - dary.length())).getString("movieNm");
+//
+//               String nAPI = NaverAPI.getUrl(movieNm);
+//
+//               JSONObject nr = new JSONObject(nAPI);
+//               JSONArray items = nr.getJSONArray("items");
+//
+//               movieVO.setMovieNM(movieNm);
+//               movieVO.setRank(wary.getJSONObject((i - dary.length())).getInt("rank"));
+//
+//               movieVO.setPubDate(items.getJSONObject(0).getInt("pubDate"));
+//               movieVO.setSubtitle(items.getJSONObject(0).getString("subtitle"));
+//               movieVO.setUserRating(items.getJSONObject(0).getDouble("userRating"));
+//               movieVO.setDirector(items.getJSONObject(0).getString("director"));
+//               movieVO.setActor(items.getJSONObject(0).getString("actor"));
+//               movieVO.setImgUrl(items.getJSONObject(0).getString("image"));
+//
+//
+//               movielist.add(movieVO);
+//           }
+//
+//        s.setAttribute("movielist", movielist);
+//
+//        } catch (JSONException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return "login";
+//    }
+//}
 
